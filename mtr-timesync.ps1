5f61d8a1d136ef2c08cffa41968e2f8648ff3b39
Rob M <robertomoir@users.noreply.github.com>
2021-02-26 10:57:51 +0000

    Update mtr-timesync.ps1

--- mtr-timesync.ps1
+++ mtr-timesync.ps1
@@ -1,10 +1,11 @@
-﻿<# Teams Meeting Room Time fix - rob 3/2/2021 v1.1 Powershell edition
+﻿<# Teams Meeting Room Time fix - rob 3/2/2021 v1.2 Powershell edition
 Provides reliable time for Teams Meeting Rooms that are Azure AD joined by doing the following
 1) Update the service trigger for W32Time to be the presence of a network connection (the default is 'on domain join')
 2) Update the service to use time.windows.com as time source.
 3) Restart the w32Time service and force a time sync
 
 n.b. if you're testing this on a VM, disable your VM tools/additions/integrations native time sync first... *sigh*
+v1.2 change to update time server list to use pool.ntp.org
 #>
  
 sc.exe triggerinfo w32time start/networkon stop/networkoff
